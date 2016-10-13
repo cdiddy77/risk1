ed0dc4084725682cbcb1a95c45336b3c50b4d69c
Seamus Parker <seamus@parkerfolk.net>
2016-10-12 22:52:45 -0700

    nextPhase, undo, better card exchange

--- web/game.ts
+++ web/game.ts
@@ -71,31 +71,110 @@ class Player {
     hand: Card[] = [];
     order: number;
     static nextPlayer() {
-        console.log('yep');
-        if (currentPlayer.order == players.length - 1) {
-            if (currentPhase == 3) {
-                for (let i = 0; i < players.length; i++){
-                    if (players[i].captured){
-                        giveCard(players[i]);
-                    }
-                }
-                currentPhase = 1;
-            }
-            else if (currentPhase == 0) { }
-            else currentPhase++;
+        if (currentPlayer == null) {
             currentPlayer = players[0];
+        }
+        else if (currentPhase == 3) {
 
-        } else currentPlayer = players[currentPlayer.order + 1];
+            if (currentPlayer.captured) {
+                giveCard(currentPlayer);
+            }
+
+            currentPhase = 1;
+
+            if (currentPlayer == players[players.length - 1]) {
+                currentPlayer = players[0];
+            } else currentPlayer = players[currentPlayer.order + 1];
+        } else currentPhase++;
         hasInit = false;
         $('#hand').html("<button id='hand' type='button' class='btn btn-primary'>my hand: </button>");
-        if(currentPhase == 1){
-            for (let i = 0; i < currentPlayer.hand.length; i++){
-                $('#hand').append('<button id="'+currentPlayer.hand[i].region.name+'" class="btn btn-primary" onclick="'
-                 + (init(deck[deck.length - 1])) + '">' + currentPlayer.hand[i].region.name + '</button>');
+        if (currentPhase == 1) {
+            for (let i = 0; i < currentPlayer.hand.length; i++) {
+                $('#hand').append('<button id="' + currentPlayer.hand[i].region.name + '" class="btn btn-primary" onclick="'
+                    + (init(deck[deck.length - 1])) + '">' + currentPlayer.hand[i].region.name + '</button>');
             }
         }
+        if (currentPhase == 1) {
+            actions.splice(0, actions.length);
+            $('#undo').removeClass('hidden');
+            if (hasInit == false) {
+                unitPool = 3;
+                var totalRegions: number;
+                totalRegions = 0;
+                for (let i = 0; i < model.allRegions.length; i++) {
+                    if (model.allRegions[i].team == currentPlayer.team) {
+                        totalRegions++;
+                    }
+
+                }
+
+                hasAll = true;
+                for (let i = 0; i < continents.length; i++) {
+                    hasAll = true;
+                    for (let j = 0; j < continents[i].territories.length; j++) {
+                        for (let k = 0; k < model.allRegions.length; k++) {
+                            if (model.allRegions[k].name == continents[i].territories[j]) {
+                                index = k;
+                                console.log(index);
+                            }
+                        }
+                        if (model.allRegions[index].team != currentPlayer.team) {
+                            hasAll = false;
+                            console.log('nope');
+                        }
+
+                    }
+                    if (hasAll == true) {
+                        unitPool += continents[i].ownershipPoints;
+                        console.log('aaaaa');
+                    }
+                }
+                if (totalRegions >= 12 && totalRegions <= 14) {
+                    unitPool += 1;
+                }
+                if (totalRegions >= 15 && totalRegions <= 17) {
+                    unitPool += 2;
+                }
+                if (totalRegions >= 18 && totalRegions <= 20) {
+                    unitPool += 3;
+                }
+                if (totalRegions >= 21 && totalRegions <= 23) {
+                    unitPool += 4;
+                }
+                if (totalRegions >= 24 && totalRegions <= 26) {
+                    unitPool += 5;
+                }
+                if (totalRegions >= 27 && totalRegions <= 29) {
+                    unitPool += 6;
+                }
+                if (totalRegions >= 30 && totalRegions <= 32) {
+                    unitPool += 7;
+                }
+                if (totalRegions >= 33 && totalRegions <= 35) {
+                    unitPool += 8;
+                }
+                if (totalRegions >= 36 && totalRegions <= 39) {
+                    unitPool += 9;
+                }
+                if (totalRegions >= 40 && totalRegions <= 42) {
+                    unitPool += 10;
+                }
+                hasInit = true;
+            }
+            $('#battleBox').text(currentPlayer.team + ', you have ' + unitPool + ' units to spend');
+        }
+        if (currentPhase == 3) {
+            $('#undo').addClass('hidden');
+            $('#battleBox').text(currentPlayer.team + ', move your units');
+        }
+
+        if (currentPhase == 2 && selectedRegion == null) {
+            $('#battleBox').text(currentPlayer.team + ' is on the attack!');
+            $('#undo').addClass('hidden');
+        }
+
     }
-    
+
 }
 class Continent {
     constructor(n: string, r: GameRegion[], p: number) {
@@ -110,6 +189,15 @@ class Continent {
 $('#hand').html("<button id='hand' type='button' class='btn btn-primary'>my hand: </button>");
 $('#warning').addClass('hidden');
 $('#attackButton').addClass('hidden');
+$('#undo').click(function (ev) {
+    if (actions.length > 0) {
+        actions[actions.length - 1].currentUnits--;
+        unitPool++;
+        actions.splice(actions.length - 1, 1);
+        $('#battleBox').text(currentPlayer.team + ', you have ' + unitPool + ' units to spend');
+        queueRedraw();
+    } else $('#battleBox').text('you havent done anything');
+})
 var turn: number = 1;
 canvas.onmousedown = function (ev: MouseEvent) {
     if (currentPhase == 0) {
@@ -156,21 +244,24 @@ canvas.onmousedown = function (ev: MouseEvent) {
 
     }
     else if (currentPhase == 1) {
-        if (unitPool > 0) {
-            if (model.hoverRegion == null) {
-                $('#warning').removeClass('hidden');
-            }
-            else if (model.hoverRegion.team != currentPlayer.team) {
-                $('#warning').removeClass('hidden');
-            }
-            else if (model.hoverRegion.team == currentPlayer.team) {
-                unitPool--;
-                model.hoverRegion.currentUnits++;
-                $('#warning').addClass('hidden');
-            }
-        } else $('#warning').removeClass('hidden');
-        $('#battleBox').text(currentPlayer.team + ', you have ' + unitPool + ' units to spend');
+        giveCard(currentPlayer);
+        if (unitPool == 0) { $('#battleBox').text('nothing left to place'); }
+        else if (model.hoverRegion == null) {
+            $('#warning').removeClass('hidden');
+        }
+        else if (model.hoverRegion.team != currentPlayer.team) {
+            $('#warning').removeClass('hidden');
+        }
+        else if (model.hoverRegion.team == currentPlayer.team) {
+            unitPool--;
+            model.hoverRegion.currentUnits++;
+            actions[actions.length] = model.hoverRegion;
+            $('#warning').addClass('hidden');
+        }
 
+        if (currentPhase == 1) {
+            $('#battleBox').text(currentPlayer.team + ', you have ' + unitPool + ' units to spend');
+        }
     }
     else if (currentPhase == 2) {
         if (model.hoverRegion != null) {
@@ -191,41 +282,105 @@ canvas.onmousedown = function (ev: MouseEvent) {
     queueRedraw();
 }
 $('#tradein').addClass('hidden');
-$('#tradein').click (function (ev) {
-    if (currentPhase == 1){
+$('#tradein').click(function (ev) {
+    if (currentPhase == 1) {
         cardPoints = 0;
-        for (let i = 0; i < tradeIns.length; i++){
+        for (let i = 0; i < tradeIns.length; i++) {
             cardPoints += tradeIns[i].stars;
         }
-        if (cardPoints == 2){
-            unitPool += 2;
+
+        if (cardPoints > 1) {
+            $('#hand').html("<button id='hand' type='button' class='btn btn-primary'>my hand: </button>");
+            for (let i = 0; i < currentPlayer.hand.length; i++) {
+                if (tradeIns.indexOf(currentPlayer.hand[i]) >= 0) {
+                    currentPlayer.hand.splice(i, 1);
+                } else {
+                    $('#hand').append('<button id="' + currentPlayer.hand[i].region.name + '" class="btn btn-primary" onclick="'
+                        + (init(currentPlayer.hand[i])) + '">' + currentPlayer.hand[i].region.name + '</button>');
+                }
+
+            }
+            if (cardPoints == 2) {
+                unitPool += 2;
+            }
+            if (cardPoints == 3) {
+                unitPool += 4;
+            }
+            if (cardPoints == 4) {
+                unitPool += 7;
+            }
+            if (cardPoints == 5) {
+                unitPool += 10;
+            }
+            if (cardPoints == 6) {
+                unitPool += 13;
+            }
+            if (cardPoints == 7) {
+                unitPool += 17;
+            }
+            if (cardPoints == 8) {
+                unitPool += 21;
+            }
+            if (cardPoints == 9) {
+                unitPool += 25;
+            }
+            if (cardPoints >= 10) {
+                unitPool += 30;
+            }
+            tradeIns.splice(0, tradeIns.length);
+            $('#tradein').text('trade in: ');
+            for (let i = 0; i < tradeIns.length; i++) {
+                $('#tradein').append(tradeIns[i].region.name + ', ');
+            }
         }
-        if (cardPoints == 3){
-            unitPool += 4;
+    }
+    $('#battleBox').text(currentPlayer.team + ', you have ' + unitPool + ' units to spend');
+})
+
+$('#tradein').mouseover(function (ev) {
+    console.log('hovering');
+    $('#tradein').text('trade in: ');
+    for (let i = 0; i < tradeIns.length; i++) {
+        $('#tradein').append(tradeIns[i].region.name + ', ');
+    }
+    if (currentPhase == 1) {
+        cardPoints = 0;
+        for (let i = 0; i < tradeIns.length; i++) {
+            cardPoints += tradeIns[i].stars;
         }
-        if (cardPoints == 4){
-            unitPool += 7;
+        if (cardPoints == 1) {
+            $('#tradein').append(' (0 points)');
         }
-        if (cardPoints == 5){
-            unitPool += 10;
+        if (cardPoints == 2) {
+            $('#tradein').append(' (2 points)');
         }
-        if (cardPoints == 6){
-            unitPool += 13;
+        if (cardPoints == 3) {
+            $('#tradein').append(' (4 points)');
         }
-        if (cardPoints == 7){
-            unitPool += 17;
+        if (cardPoints == 4) {
+            $('#tradein').append(' (7 points)');
         }
-        if (cardPoints == 8){
-            unitPool += 21;
+        if (cardPoints == 5) {
+            $('#tradein').append(' (10 points)');
         }
-        if (cardPoints == 9){
-            unitPool += 25;
+        if (cardPoints == 6) {
+            $('#tradein').append(' (13 points)');
         }
-        if (cardPoints >= 10){
-            unitPool += 30;
+        if (cardPoints == 7) {
+            $('#tradein').append(' (17 points)');
+        }
+        if (cardPoints == 8) {
+            $('#tradein').append(' (21 points)');
+        }
+        if (cardPoints == 9) {
+            $('#tradein').append(' (25 points)');
+        }
+        if (cardPoints >= 10) {
+            $('#tradein').append(' (30 points)');
         }
     }
 })
+
 function moveClick() {
     if (selectedRegion == null) {
         if (model.hoverRegion.team == currentPlayer.team) {
@@ -363,32 +518,53 @@ $('#attackButton').click(function (ev) {
     }
     queueRedraw();
 });
-
 function init(card: Card) {
-    
+
     $('#tradein').removeClass('hidden');
     var clickString: string = "";
     clickString = "\
+    spliced = false;\
     for (let i = 0; i < model.allRegions.length; i++){\
     if(model.allRegions[i].name == $(this).attr('id')){\
     index = i;\
     }\
     }\
-    if (currentPhase != 1){\
+    for (let i = 0; i < tradeIns.length; i++) { \
+    if (tradeIns[i].region.name == $(this).attr('id')) {\
+    currentPlayer.hand[currentPlayer.hand.length] = tradeIns[i];\
+    tradeIns.splice(i, 1);\
+    spliced = true;\
+    $('#tradein').text('trade in: ');\
+    for (let i = 0; i < tradeIns.length; i++) {\
+    $('#tradein').append(tradeIns[i].region.name + ', ');\
+    }\
+    }\
+    }\
+    if (currentPhase != 1 && $('#battleBox').text() != 'already trading this in\!'){\
     $('#battleBox').text('no trade-ins during this phase');\
     }\
     else if (model.allRegions[index].team != currentPlayer.team){\
     $('#battleBox').text('this region doesnt belong to you\!');\
     }\
-    else if (tradeIns.indexOf($(this).attr('id')) >= 0){ \
-    $('#battleBox').text('already trading this in'); \
-    } \
     else{ \
-    $('#tradein').append($(this).attr('id')+', '); \
-    tradeIns[tradeIns.length] = $(this); \
+    if (spliced == false){\
+    $('#tradein').append($(this).attr('id')+', ');\
+    for (let i = 0; i < currentPlayer.hand.length; i++){\
+    if (currentPlayer.hand[i].region.name == $(this).attr('id')){\
+    index = i;\
+    }\
+    }\
+    tradeIns[tradeIns.length] = currentPlayer.hand[index]; \
+    $('#tradein').text('trade in: ');\
+    for (let i = 0; i < tradeIns.length; i++) {\
+    $('#tradein').append(tradeIns[i].region.name + ', ');\
+    }\
+    }\
     }"
     return (clickString);
 }
+$('#battleBox').text
+var spliced: boolean;
 var index: number;
 var hasCard: boolean = false;
 var tradeIns: Card[] = [];
@@ -409,6 +585,7 @@ var hasInit: boolean = false;
 var hasAll: boolean = true;
 var index1: number;
 var cardPoints: number = 0;
+var actions: GameRegion[] = [];
 $(() => {
     console.log('we were here');
     img = <HTMLImageElement>document.getElementById('riskmap');
@@ -419,82 +596,7 @@ $(() => {
     }, 1000);
 
     canvas.onmousemove = function (ev: MouseEvent) {
-        if (currentPhase == 3){
-            $('#battleBox').text(currentPlayer.team+', move your units');
-        }
-        for (let i = 0; i < currentPlayer.hand.length; i++) {
 
-        }
-        if (currentPhase == 1) {
-            if (hasInit == false) {
-                unitPool = 3;
-                var totalRegions: number;
-                totalRegions = 0;
-                for (let i = 0; i < model.allRegions.length; i++) {
-                    if (model.allRegions[i].team == currentPlayer.team) {
-                        totalRegions++;
-                    }
-
-                }
-               
-                hasAll = true;
-                for (let i = 0; i < continents.length; i++){
-                    hasAll = true;
-                    for (let j = 0; j < continents[i].territories.length; j++){
-                        for (let k = 0; k < model.allRegions.length; k++){
-                            if (model.allRegions[k].name == continents[i].territories[j]){
-                                index = k;
-                                console.log(index);
-                            }
-                        }
-                        if (model.allRegions[index].team != currentPlayer.team){
-                            hasAll = false;
-                            console.log('nope');
-                        }
-                        
-                    }
-                    if (hasAll == true){
-                        unitPool += continents[i].ownershipPoints;
-                        console.log('aaaaa');
-                    }
-                }
-                if (totalRegions >= 12 && totalRegions <= 14) {
-                    unitPool += 1;
-                }
-                if (totalRegions >= 15 && totalRegions <= 17) {
-                    unitPool += 2;
-                }
-                if (totalRegions >= 18 && totalRegions <= 20) {
-                    unitPool += 3;
-                }
-                if (totalRegions >= 21 && totalRegions <= 23) {
-                    unitPool += 4;
-                }
-                if (totalRegions >= 24 && totalRegions <= 26) {
-                    unitPool += 5;
-                }
-                if (totalRegions >= 27 && totalRegions <= 29) {
-                    unitPool += 6;
-                }
-                if (totalRegions >= 30 && totalRegions <= 32) {
-                    unitPool += 7;
-                }
-                if (totalRegions >= 33 && totalRegions <= 35) {
-                    unitPool += 8;
-                }
-                if (totalRegions >= 36 && totalRegions <= 39) {
-                    unitPool += 9;
-                }
-                if (totalRegions >= 40 && totalRegions <= 42) {
-                    unitPool += 10;
-                }
-                hasInit = true;
-            }
-            $('#battleBox').text(currentPlayer.team + ', you have ' + unitPool + ' units to spend');
-        }
-        if (currentPhase == 2 && selectedRegion == null) {
-            $('#battleBox').text(currentPlayer.team + ' is on the attack!');
-        }
         var pt = Point.parse(ev);
         if (model.activeDrawRegion != null) {
             model.currentMousePoint = pt;
@@ -515,7 +617,9 @@ $(() => {
         players[0].order = 0;
         players[1].order = 1;
         players[2].order = 2;
+        queueRedraw();
         setup();
+
     });
 
     $('#fourPlayers').click(function (ev) {
@@ -531,7 +635,9 @@ $(() => {
         players[1].order = 1;
         players[2].order = 2;
         players[3].order = 3;
+        queueRedraw();
         setup();
+
     });
     $('#fivePlayers').click(function (ev) {
         players.length = 5;
@@ -548,6 +654,7 @@ $(() => {
         players[2].order = 2;
         players[3].order = 3;
         players[4].order = 4;
+        queueRedraw();
         setup();
     });
     //how do i make a comment??
@@ -565,8 +672,7 @@ $.getJSON('continents.json', function (data) {
 var deck: Card[] = [];
 function setup() {
     $('#attackButton').addClass('hidden');
-    console.log("check1");
-    currentPlayer = players[0];
+
 
     for (var i = 0; i < model.allRegions.length; i++) {
         deck[i] = new Card(model.allRegions[i], model.allRegions[i].startingUnits)
@@ -592,6 +698,7 @@ function setup() {
             deck[index] = temp;
         }
     }
+    Player.nextPlayer();
 }
 if (classic == true) {
     currentPhase = 0;
@@ -609,8 +716,8 @@ if (classic == true) {
 } else currentPhase = 1;
 function giveCard(to: Player) {
     to.hand[to.hand.length] = deck[deck.length - 1];
-    $('#hand').append('<button id="'+deck[deck.length - 1].region.name+'" class="btn btn-primary" onclick="'
-     + (init(deck[deck.length - 1])) + '">' + deck[deck.length - 1].region.name + '</button>');
+    $('#hand').append('<button id="' + deck[deck.length - 1].region.name + '" class="btn btn-primary" onclick="'
+        + (init(deck[deck.length - 1])) + '">' + deck[deck.length - 1].region.name + '</button>');
     deck.splice(deck.length - 1, 1);
 
 }
